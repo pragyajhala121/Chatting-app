048f76be6303055100ae4d368718ba31bad6730e
pragyajhala121 <73744592+pragyajhala121@users.noreply.github.com>
2021-01-13 19:48:32 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,7 +1,8 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
     apiKey: "AIzaSyAwCf4uY770z9LmKNukjE9lraA9PwMav0Q",
     authDomain: "chatapp-d7a02.firebaseapp.com",
+    databaseURL: "https://chatapp-d7a02-default-rtdb.firebaseio.com",
     projectId: "chatapp-d7a02",
     storageBucket: "chatapp-d7a02.appspot.com",
     messagingSenderId: "630246564324",
